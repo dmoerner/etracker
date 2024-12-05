8bdae7e21e3e272ae9e394adaa03bab9ee44ba4a
Daniel Moerner <dmoerner@gmail.com>
2024-12-05 12:38:42 -0500

    Initialize info_hash index and last_announce timestamp updates

--- etracker.go
+++ etracker.go
@@ -48,6 +48,7 @@ func dbConnect() (*pgxpool.Pool, error) {
 	}
 	defer dbpool.Close()
 
+	// cf. https://x-team.com/blog/automatic-timestamps-with-postgresql
 	_, err = dbpool.Exec(ctx, `
 		CREATE TABLE IF NOT EXISTS peers (
 			id SERIAL PRIMARY KEY,
@@ -56,6 +57,21 @@ func dbConnect() (*pgxpool.Pool, error) {
 			info_hash VARCHAR(20) NOT NULL,
 			last_announce TIMESTAMPTZ NOT NULL DEFAULT NOW()
 		);
+
+		CREATE INDEX IF NOT EXISTS idx_info_hash ON peers(info_hash);
+
+		CREATE OR REPLACE FUNCTION trigger_set_timestamp()
+		RETURNS TRIGGER AS $$
+		BEGIN
+			NEW.last_announce = NOW();
+			RETURN NEW;
+		END;
+		$$ LANGUAGE plpgsql;
+
+		CREATE OR REPLACE TRIGGER set_timestamp
+		BEFORE UPDATE ON peers
+		FOR EACH ROW
+		EXECUTE PROCEDURE trigger_set_timestamp();
 	`)
 
 	if err != nil {
