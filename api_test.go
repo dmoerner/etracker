3eadc89fdce8c4e7971a947b669d50461f5c72a3
Daniel Moerner <dmoerner@gmail.com>
2024-12-16 15:51:18 -0500

    Add tests for when API authorization is unset
    These are essential tests due to the lack of optional types, and must
    always pass. Otherwise, there is the risk that an empty string in the
    Authorization Header will be incorrectly interpreted as a valid
    authorization.

--- api_test.go
+++ api_test.go
@@ -5,7 +5,43 @@ import (
 	"testing"
 )
 
-func TestAuthorization(t *testing.T) {
+// TestUnsetAuthorization is a critical test. Due to the lack of optional
+// types, an empty authorization string in the config struct is required by the
+// program to reject all API attempts, including with an empty string in
+// the Authorization Header.
+func TestUnsetAuthorization(t *testing.T) {
+	config := buildTestConfig(defaultAlgorithm, "")
+
+	data := []struct {
+		name          string
+		request       string
+		authorization string
+		expected      int
+	}{
+		{"normally good api key", "http://example.com:8080/api", defaultAPIKey, 403},
+		{"bad api key", "http://example.com:8080/api", "badapikey", 403},
+		{"no api key", "http://example.com:8080/api", "", 403},
+	}
+
+	handler := APIHandler(config)
+
+	for _, d := range data {
+		t.Run(d.name, func(t *testing.T) {
+			req := httptest.NewRequest("GET", d.request, nil)
+			req.Header.Add("Authorization", d.authorization)
+			w := httptest.NewRecorder()
+
+			handler(w, req)
+			if w.Result().StatusCode != d.expected {
+				t.Errorf("expected %d, got %d", d.expected, w.Result().StatusCode)
+			}
+		})
+	}
+
+	teardownTest(config)
+}
+
+func TestAuthorizationHeader(t *testing.T) {
 	config := buildTestConfig(defaultAlgorithm, defaultAPIKey)
 
 	data := []struct {
