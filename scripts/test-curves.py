61f655c792d5672193385386d5a03c96d0ca57a9
Daniel Moerner <dmoerner@gmail.com>
2025-01-20 11:31:36 -0500

    Update test-curves.py with dynamic calculation of steepness
    Calculate the steepness as a function of a target value for seeding,
    with an appropriate delta since the function is asymptotic. Thank you to
    Farid Rener for pairing on some of this code.

--- scripts/test-curves.py
+++ scripts/test-curves.py
@@ -2,28 +2,39 @@
 #
 # Python helper function to more easily visualize various possible smoothing
 # functions for the peer distribution.
+#
+# There is no guarantee that the functions and constants here match
+# algorithms.go
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-# import math
 
+def smooth_function(x, numwant, good_seedcount):
+    """
+    Current function:
+    y = y_0 + (numwant - y_0) * tanh(kx)
+    k is the steepness, and it's calculated such that if a client is seeding
+    more than 1 standard deviation above the average seed count, they get the
+    maximum they requested.
+    """
 
-# def smooth_function(x, y_0, n, k=None):
-#     return y_0 + (n - 5) / (1 + (math.e) ** (-x))
+    # minimum peer count to distribute
+    y_0 = 5
+
+    # atanh(1) is infinite since the function is asymptotic, so we need to
+    # calculate the steepness relative to a delta.
+    delta = 0.1
+
+    k = np.arctanh((numwant - delta - y_0) / (numwant - y_0)) / good_seedcount
+
+    return y_0 + (numwant - y_0) * (np.tanh(k * x))
+
 
 # Parameters
-y_0 = 5  # Intercept above 0
-n = 50  # Target value
-k = 0.05  # Steepness
 x = np.linspace(0, 200)  # x-values
 
-
-def smooth_function(x, y_0, n, k):
-    return y_0 + (n - y_0) * (np.tanh(k * x))
-
-
-y = smooth_function(x, y_0, n, k)
+y = smooth_function(x, 50, 100)
 
 # Plot the result
 plt.plot(x, y)
